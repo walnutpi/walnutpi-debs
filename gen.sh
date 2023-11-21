93efa41419bc106a5fe1c50309ece938e1d5f3a9
sc-bin <3335447573@qq.com>
2023-11-21 14:49:47 +0800

    保存一批deb包

--- gen.sh
+++ gen.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-OUTPUT="output"
+PATH_PWD="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
+OUTPUT="${PATH_PWD}/output"
 
 if [[ -d $OUTPUT ]]; then
     rm -r  $OUTPUT
@@ -23,17 +24,18 @@ cp arm64/output/*.deb ${OUTPUT}/arm64/
 #################################
 #           生成dists           #
 #################################
+cd ${OUTPUT}
 bin_all=${OUTPUT}/dists/bookworm/main/binary-all
 bin_arm64=${OUTPUT}/dists/bookworm/main/binary-arm64
 mkdir  -p ${bin_all}
 mkdir  -p ${bin_arm64}
-dpkg-scanpackages ${OUTPUT}/main  /dev/null | gzip> ${bin_all}/Packages.gz 
-dpkg-scanpackages ${OUTPUT}/arm64  /dev/null | gzip> ${bin_arm64}/Packages.gz 
+dpkg-scanpackages main  /dev/null | gzip> ${bin_all}/Packages.gz 
+dpkg-scanpackages arm64  /dev/null | gzip> ${bin_arm64}/Packages.gz 
 
 gzip -c -d ${bin_all}/Packages.gz  > ${bin_all}/Packages
 gzip -c -d ${bin_arm64}/Packages.gz  > ${bin_arm64}/Packages
 
-
+cd $PATH_PWD
 apt-ftparchive -c=bookworm.conf release ${OUTPUT}/dists/bookworm > ${OUTPUT}/dists/bookworm/Release
 
 
