107f811e50bb958c8395035ce69ddac576c5e12e
sc-bin <3335447573@qq.com>
2023-11-23 17:54:37 +0800

    添加大量包

--- arm64/gen.sh
+++ arm64/gen.sh
@@ -4,6 +4,7 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 OUTPUT_DIR="${SCRIPT_DIR}/output"
+SAVE_DIR="${SCRIPT_DIR}/save"
 
 if [[ -d $OUTPUT_DIR ]]; then
     rm -r  $OUTPUT_DIR
@@ -12,21 +13,27 @@ fi
 if [[ ! -d ${OUTPUT_DIR} ]]; then
     mkdir ${OUTPUT_DIR}
 fi
+if [[ -d $SAVE_DIR ]]; then
+    cp ${SAVE_DIR}/*  ${OUTPUT_DIR}
+fi
+
 
 # 遍历当前目录下的所有文件夹
 for dir in ${SCRIPT_DIR}/*/ ; do
     if [[ $dir == ${OUTPUT_DIR}/ ]]; then
         continue
     fi
-
-    cd "$dir"
+    if [[ $dir == ${SAVE_DIR}/ ]]; then
+        continue
+    fi
+    cd $dir
     
     # 检查DEBIAN/gen.sh文件是否存在，如果存在就运行它
     if [[ -f DEBIAN/gen.sh ]]; then
         cd DEBIAN
-        bash DEBIAN/gen.sh
+        bash gen.sh
+        cd $dir
     fi
-    
     package_name=$(grep -oP '(?<=Package: ).*' DEBIAN/control)
     version=$(grep -oP '(?<=Version: ).*' DEBIAN/control)
     architecture=$(grep -oP '(?<=Architecture: ).*' DEBIAN/control)
--- main/gen.sh
+++ main/gen.sh
@@ -4,6 +4,7 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 OUTPUT_DIR="${SCRIPT_DIR}/output"
+SAVE_DIR="${SCRIPT_DIR}/save"
 
 if [[ -d $OUTPUT_DIR ]]; then
     rm -r  $OUTPUT_DIR
@@ -12,13 +13,19 @@ fi
 if [[ ! -d ${OUTPUT_DIR} ]]; then
     mkdir ${OUTPUT_DIR}
 fi
+if [[ -d $SAVE_DIR ]]; then
+    cp ${SAVE_DIR}/*  ${OUTPUT_DIR}
+fi
+
 
 # 遍历当前目录下的所有文件夹
 for dir in ${SCRIPT_DIR}/*/ ; do
     if [[ $dir == ${OUTPUT_DIR}/ ]]; then
         continue
     fi
-    
+    if [[ $dir == ${SAVE_DIR}/ ]]; then
+        continue
+    fi
     cd $dir
     
     # 检查DEBIAN/gen.sh文件是否存在，如果存在就运行它
