331b5ffb3555b5f7749f8ae95b73f27166bb0cc0
sc-bin <3335447573@qq.com>
2023-12-14 15:07:52 +0800

    优化gen脚本，生成包前会比较包与文件夹的修改时间

--- arm64/gen.sh
+++ arm64/gen.sh
@@ -6,17 +6,9 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 OUTPUT_DIR="${SCRIPT_DIR}/output"
 SAVE_DIR="${SCRIPT_DIR}/save"
 
-if [[ -d $OUTPUT_DIR ]]; then
-    rm -r  $OUTPUT_DIR
-fi
+[[ ! -d ${OUTPUT_DIR} ]] && mkdir ${OUTPUT_DIR}
 
-if [[ ! -d ${OUTPUT_DIR} ]]; then
-    mkdir ${OUTPUT_DIR}
-fi
-if [[ -d $SAVE_DIR ]]; then
-    cp ${SAVE_DIR}/*  ${OUTPUT_DIR}
-fi
-
+[[ -d $SAVE_DIR ]] && cp ${SAVE_DIR}/*  ${OUTPUT_DIR}
 
 # 遍历当前目录下的所有文件夹
 for dir in ${SCRIPT_DIR}/*/ ; do
@@ -38,10 +30,28 @@ for dir in ${SCRIPT_DIR}/*/ ; do
     version=$(grep -oP '(?<=Version: ).*' DEBIAN/control)
     architecture=$(grep -oP '(?<=Architecture: ).*' DEBIAN/control)
     
-    # 计算大小写入DEBIAN/control文件的Installed-Size:行
+    # 计算大小
     size=$(du -sk --exclude=DEBIAN . | cut -f1)
-    sed -i "/Installed-Size:/c\Installed-Size: $size" DEBIAN/control
+    
+    # 获取DEBIAN/control文件中的Installed-Size:行的值
+    old_size=$(grep -oP '(?<=Installed-Size: ).*' DEBIAN/control)
+    
+    # 如果新的大小和旧的大小不同，就写入新的大小
+    if [[ $size != $old_size ]]; then
+        sed -i "/Installed-Size:/c\Installed-Size: $size" DEBIAN/control
+    fi
+    
+    deb_file="${OUTPUT_DIR}/${package_name}_${version}_${architecture}.deb"
+    
+    # 检查deb包是否存在，如果存在并且生成时间晚于文件夹的修改时间，就跳过这个包
+    if [[ -f $deb_file ]]; then
+        deb_time=$(stat -c %Y "$deb_file")
+        dir_time=$(find "$dir" -type f -exec stat -c %Y {} \; | sort -nr | head -1)
+        if (( deb_time >= dir_time )); then
+            continue
+        fi
+    fi
     
     cd ..
-    dpkg -b "$dir" "${OUTPUT_DIR}/${package_name}_${version}_${architecture}.deb"
+    dpkg -b "$dir" "$deb_file"
 done
