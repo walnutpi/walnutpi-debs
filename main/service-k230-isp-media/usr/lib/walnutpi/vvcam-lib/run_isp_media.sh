e16517585d5e1c48cd14fe22db09cddaf5c7d7df
sc-bin <3335447573@qq.com>
2026-07-31 17:40:11 +0800

    [update] service-k230-isp-media 修复启动失败的问题

--- main/service-k230-isp-media/usr/lib/walnutpi/vvcam-lib/run_isp_media.sh
+++ main/service-k230-isp-media/usr/lib/walnutpi/vvcam-lib/run_isp_media.sh
@@ -13,7 +13,7 @@ if [ -w /proc/sys/kernel/sched_rt_runtime_us ]; then
 	echo -1 > /proc/sys/kernel/sched_rt_runtime_us
 fi
 
-ISP_MEDIA_SENSOR_DRIVER=/usr/lib/libvvcam.so "${SCRIPT_DIR}/isp_media_server" >/tmp/isp.err.log 2>&1 
+ISP_MEDIA_SENSOR_DRIVER=/usr/lib/libvvcam.so "${SCRIPT_DIR}/isp_media_server" >/tmp/isp.err.log 2>&1 &
 sleep 1
 echo 0 mode=1 > /proc/vsi/isp_subdev0
 echo 1 mode=1 > /proc/vsi/isp_subdev0
